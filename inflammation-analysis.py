ef78df7074bf73a306fe127d61e88a0f3b4210d6
Steve Crouch <s.crouch@software.ac.uk>
2024-10-25 12:48:44 +0100

    Initialize full-data-analysis

---
+++ inflammation-analysis.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""Software for managing and analysing patients' inflammation data in our imaginary hospital."""
+
+import argparse
+import os
+
+from inflammation import models, views
+from inflammation.compute_data import analyse_data
+
+
+def main(args):
+    """The MVC Controller of the patient inflammation data system.
+
+    The Controller is responsible for:
+    - selecting the necessary models and views for the current task
+    - passing data between models and views
+    """
+    infiles = args.infiles
+    if not isinstance(infiles, list):
+        infiles = [args.infiles]
+
+
+    if args.full_data_analysis:
+        analyse_data(os.path.dirname(infiles[0]))
+        return
+
+    for filename in infiles:
+        inflammation_data = models.load_csv(filename)
+
+        view_data = {
+            'average': models.daily_mean(inflammation_data),
+            'max': models.daily_max(inflammation_data),
+            'min': models.daily_min(inflammation_data)
+        }
+
+        views.visualize(view_data)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='A basic patient inflammation data management system')
+
+    parser.add_argument(
+        'infiles',
+        nargs='+',
+        help='Input CSV(s) containing inflammation series for each patient')
+
+    parser.add_argument(
+        '--full-data-analysis',
+        action='store_true',
+        dest='full_data_analysis')
+
+    args = parser.parse_args()
+
+    main(args)
---
+++ inflammation/compute_data.py
@@ -0,0 +1,31 @@
+"""Module containing mechanism for calculating standard deviation between datasets.
+"""
+
+import glob
+import os
+import numpy as np
+
+from inflammation import models, views
+
+
+def analyse_data(data_dir):
+    """Calculates the standard deviation by day between datasets.
+
+    Gets all the inflammation data from CSV files within a directory,
+    works out the mean inflammation value for each day across all datasets,
+    then plots the graphs of standard deviation of these means."""
+    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
+    if len(data_file_paths) == 0:
+        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
+    data = map(models.load_csv, data_file_paths)
+
+
+    means_by_day = map(models.daily_mean, data)
+    means_by_day_matrix = np.stack(list(means_by_day))
+
+    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+
+    graph_data = {
+        'standard deviation by day': daily_standard_deviation,
+    }
+    views.visualize(graph_data)
